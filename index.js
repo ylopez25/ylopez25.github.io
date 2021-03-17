0ac3a1fc05f2b14d131ac664e7be40dc6b8a8ba3
Yesenia L <ylopezcr123@gmail.com>
2021-03-17 19:54:40 -0400

    almost

--- index.js
+++ index.js
@@ -1,3 +1,45 @@
+document.addEventListener("DOMContentLoaded", () => {
+  let homeBtn = document.getElementById("about-me")
+  homeBtn.addEventListener("click", () => {
+    let homeIcon = document.getElementById("scroll")
+    homeIcon.scrollIntoView({
+      behavior: "smooth",
+      block: "center",
+      inline:"center",
+    });
+  })
+
+  let prjBtn = document.getElementById("projects")
+  prjBtn.addEventListener("click", () => {
+    let prjIcon = document.getElementById("scrolls")
+    prjIcon.scrollIntoView({
+      behavior: "smooth",
+      block: "center",
+      inline:"center",
+    });
+  })
+
+  let contactBtn = document.getElementById("contact")
+  contactBtn.addEventListener("click", () => {
+    let contactIcon = document.getElementById("scrolled")
+    contactIcon.scrollIntoView({
+      behavior:"smooth",
+      block: "center",
+      inline:"center"
+    })
+  })
+
+  let skillsBtn = document.getElementById("skills")
+  skillsBtn.addEventListener("click", () => {
+    let skillsIcon = document.getElementById("coursel")
+    skillsIcon.scrollIntoView({
+      behavior: "smooth",
+      block: "center",
+      inline:"center",
+    });
+  })
+})
+
 function myFunction() {
   var x = document.getElementById("myDIV");
   if (x.style.display === "none") {
@@ -6,6 +48,7 @@ function myFunction() {
     x.style.display = "none";
   }
 }
+
 
 function laFunction() {
   var y = document.getElementById("laDIV");
